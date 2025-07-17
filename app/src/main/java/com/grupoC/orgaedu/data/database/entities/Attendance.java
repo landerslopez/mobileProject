e8ef761ae186648bef647f145146dca708adcc7e
Jean1820 <jeampier1820@hotmail.com>
2025-07-16 19:50:41 -0500

    asistencia 1.2

--- app/src/main/java/com/grupoC/orgaedu/data/database/entities/Attendance.java
+++ app/src/main/java/com/grupoC/orgaedu/data/database/entities/Attendance.java
@@ -3,37 +3,39 @@ package com.grupoC.orgaedu.data.database.entities;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
 import static androidx.room.ForeignKey.CASCADE;
 
-@Entity(tableName = "asistencias",
+@Entity(
+        tableName = "asistencias",
         foreignKeys = {
-                @ForeignKey(entity = User.class,
+                @ForeignKey(
+                        entity = User.class,
                         parentColumns = "id",
                         childColumns = "id_usuario",
-                        onDelete = CASCADE),
-                // @ForeignKey(entity = Course.class, // Asumiendo que tendrás una entidad Course
-                //             parentColumns = "id",
-                //             childColumns = "id_curso",
-                //             onDelete = CASCADE)
-        })
+                        onDelete = CASCADE
+                )
+        },
+        indices = {
+                @Index(value = {"id_usuario"})
+        }
+)
 public class Attendance {
+
     @PrimaryKey(autoGenerate = true)
     private int id;
 
     @ColumnInfo(name = "id_usuario")
     private int idUsuario;
 
-    // @ColumnInfo(name = "id_curso") // Si asocias la asistencia a un curso
-    // private int idCurso;
-
     @ColumnInfo(name = "fecha")
-    private Date fecha; // Usaremos TypeConverter para Date
+    private Date fecha;
 
-    @ColumnInfo(name = "estado") // Por ejemplo: "presente", "ausente", "tardanza"
+    @ColumnInfo(name = "estado")
     private String estado;
 
     // Getters y setters
@@ -42,9 +44,6 @@ public class Attendance {
 
     public int getIdUsuario() { return idUsuario; }
     public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
-
-    // public int getIdCurso() { return idCurso; }
-    // public void setIdCurso(int idCurso) { this.idCurso = idCurso; }
 
     public Date getFecha() { return fecha; }
     public void setFecha(Date fecha) { this.fecha = fecha; }
